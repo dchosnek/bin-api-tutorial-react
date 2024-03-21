6599a9bbc934850f66cc93861bbc400bd63172e1
Doron Chosnek <my.random.items@gmail.com>
2024-03-21 17:21:16 -0500

    added navbar

--- src/App.js
+++ src/App.js
@@ -2,7 +2,11 @@ import './App.css';
 import BinTable from './bin-table';
 import TokenForm from './fetch-token';
 import Container from 'react-bootstrap/Container';
+import Navbar from 'react-bootstrap/Navbar';
+import Nav from 'react-bootstrap/Nav';
 import { useState } from 'react';
+
+import { InboxesFill, LockFill, UnlockFill } from 'react-bootstrap-icons';
 
 function App() {
 
@@ -10,16 +14,38 @@ function App() {
 
   return (
     <Container>
-      <div className="App">
+
+      {/* NAVBAR */}
+      <Navbar expand="lg" bg='dark' data-bs-theme='dark'>
         <Container>
-          <div className='row'>
-            <TokenForm bearerToken={bearerToken} setBearerToken={setBearerToken} />
-          </div>
-          <div className={`row mt-5 ${bearerToken==='' && 'invisible'}`}>
-            <BinTable token={bearerToken} />
-          </div>
+          <Navbar.Brand href="#home"><InboxesFill /> Bins API Tutorial</Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="me-auto">
+              <Nav.Link href="#home"><LockFill hidden={bearerToken !== ''} /><UnlockFill hidden={bearerToken === ''} /> Token</Nav.Link>
+              <Nav.Link href="#bins">Bins</Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
         </Container>
-      </div>
+      </Navbar>
+      {/* END OF NAVBAR */}
+
+      <Container>
+        <div className="App">
+          <Container>
+            <div className='row'><h3>Your token</h3></div>
+            <div className='row mb-3'>Use this form to generate a token for API access.</div>
+            <div className='row'>
+              <TokenForm bearerToken={bearerToken} setBearerToken={setBearerToken} />
+            </div>
+            <div className='row mt-5'><h3>Your bins</h3></div>
+            <div className='row'>Use the table below to create (), edit, and delete () bins.</div>
+            <div className={`row mt-5 ${bearerToken === '' && 'invisible'}`}>
+              <BinTable token={bearerToken} />
+            </div>
+          </Container>
+        </div>
+      </Container>
     </Container>
   );
 }
