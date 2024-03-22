34db2be69c4fc43fe99648454b32557100aeae45
Doron Chosnek <my.random.items@gmail.com>
2024-03-21 21:58:40 -0500

    split app into two pages

--- src/App.js
+++ src/App.js
@@ -11,6 +11,7 @@ import { InboxesFill, LockFill, UnlockFill } from 'react-bootstrap-icons';
 function App() {
 
   const [bearerToken, setBearerToken] = useState('');
+  const [homePage, setHomePage] = useState(true);
 
   return (
     <Container>
@@ -18,12 +19,12 @@ function App() {
       {/* NAVBAR */}
       <Navbar expand="lg" bg='dark' data-bs-theme='dark'>
         <Container>
-          <Navbar.Brand href="#home"><InboxesFill /> Bins API Tutorial</Navbar.Brand>
+          <Navbar.Brand href="#" onClick={()=>{ setHomePage(true); }}><InboxesFill /> Bins API Tutorial</Navbar.Brand>
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
           <Navbar.Collapse id="basic-navbar-nav">
             <Nav className="me-auto">
-              <Nav.Link href="#home"><LockFill hidden={bearerToken !== ''} /><UnlockFill hidden={bearerToken === ''} /> Token</Nav.Link>
-              <Nav.Link href="#bins">Bins</Nav.Link>
+              <Nav.Link href="#" onClick={()=>{ setHomePage(true); }}><LockFill hidden={bearerToken !== ''} /><UnlockFill hidden={bearerToken === ''} /> Token</Nav.Link>
+              <Nav.Link href="#" onClick={()=>{ setHomePage(false); }} disabled={bearerToken===''}>Bins</Nav.Link>
             </Nav>
           </Navbar.Collapse>
         </Container>
@@ -31,20 +32,20 @@ function App() {
       {/* END OF NAVBAR */}
 
       <Container>
-        <div className="App">
-          <Container>
-            <div className='row'><h3>Your token</h3></div>
-            <div className='row mb-3'>Use this form to generate a token for API access.</div>
-            <div className='row'>
+          <Container hidden={!homePage}>
+            <div className='row mt-3'><h3>Your token</h3></div>
+            <div className='row'>Use this form to generate a token for API access.</div>
+            <div className='row mt-3'>
               <TokenForm bearerToken={bearerToken} setBearerToken={setBearerToken} />
             </div>
-            <div className='row mt-5'><h3>Your bins</h3></div>
+          </Container>
+          <Container hidden={homePage}>
+            <div className='row mt-3'><h3>Your bins</h3></div>
             <div className='row'>Use the table below to create (), edit, and delete () bins.</div>
-            <div className={`row mt-5 ${bearerToken === '' && 'invisible'}`}>
+            <div className='row mt-3'>
               <BinTable token={bearerToken} />
             </div>
           </Container>
-        </div>
       </Container>
     </Container>
   );
