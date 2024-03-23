d8f673d18a1d3eb71d995325f2a63323cbbbc730
Doron Chosnek <my.random.items@gmail.com>
2024-03-22 22:42:49 -0500

    added alert for no bins

--- src/bin-table/index.js
+++ src/bin-table/index.js
@@ -4,12 +4,14 @@ import Table from 'react-bootstrap/Table';
 import Button from 'react-bootstrap/Button';
 import OverlayTrigger from 'react-bootstrap/OverlayTrigger';
 import Tooltip from 'react-bootstrap/Tooltip';
+import Alert from 'react-bootstrap/Alert';
 
 import { ArrowClockwise, LightningFill, PlusLg } from 'react-bootstrap-icons';
 
 import BinRow from '../bin-row';
 
 import { API_BASE_URL } from '../constants';
+import { Container } from 'react-bootstrap';
 
 function BinTable(props) {
     const [binList, setBinList] = useState([]);
@@ -113,7 +115,7 @@ function BinTable(props) {
                 return response.json(); // Parse the response body as JSON
             })
             .then(() => {
-                const newArray = binList.map(item => 
+                const newArray = binList.map(item =>
                     item.binId === binId ? { ...item, contents: contents } : item
                 );
                 setBinList(newArray);
@@ -161,36 +163,41 @@ function BinTable(props) {
     };
 
     return (
-        <div className='row align-items-center'>
-            <div className='col'>
-                <Table hover bordered>
-                    <thead>
-                        <tr>
-                            <th className='fit-content'>binId</th>
-                            <th className='fill-space'>contents</th>
-                            <th className='action-col'><LightningFill /></th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        {binList.map(e => (
-                            <BinRow binId={e.binId} binContents={e.contents} key={e.binId} delFunc={deleteBin} saveFunc={updateBin} />
-                        ))}
-                        <tr>
-                            <td></td>
-                            <td></td>
-                            <td>
-                                <OverlayTrigger placement='auto' overlay={<Tooltip>Create new bin</Tooltip>}>
-                                    <Button variant='success' className='button-pad' onClick={createBin}><PlusLg /></Button>
-                                </OverlayTrigger>
-                                <OverlayTrigger placement='auto' overlay={<Tooltip>Refresh table</Tooltip>}>
-                                    <Button variant='primary' className='button-pad' onClick={refreshTable}><ArrowClockwise /></Button>
-                                </OverlayTrigger>
-                            </td>
-                        </tr>
-                    </tbody>
-                </Table>
+        <Container>
+            <div className='row align-items-center'>
+                <Alert key='warning' variant='warning' hidden={binList.length}>You have no bins. Press the <Button variant='success'><PlusLg /></Button> button to create a bin.</Alert>
             </div>
-        </div>
+            <div className='row align-items-center'>
+                <div className='col'>
+                    <Table hover bordered>
+                        <thead>
+                            <tr>
+                                <th className='fit-content'>binId</th>
+                                <th className='fill-space'>contents</th>
+                                <th className='action-col'><LightningFill /></th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {binList.map(e => (
+                                <BinRow binId={e.binId} binContents={e.contents} key={e.binId} delFunc={deleteBin} saveFunc={updateBin} />
+                            ))}
+                            <tr>
+                                <td></td>
+                                <td></td>
+                                <td>
+                                    <OverlayTrigger placement='auto' overlay={<Tooltip>Create new bin</Tooltip>}>
+                                        <Button variant='success' className='button-pad' onClick={createBin}><PlusLg /></Button>
+                                    </OverlayTrigger>
+                                    <OverlayTrigger placement='auto' overlay={<Tooltip>Refresh table</Tooltip>}>
+                                        <Button variant='primary' className='button-pad' onClick={refreshTable}><ArrowClockwise /></Button>
+                                    </OverlayTrigger>
+                                </td>
+                            </tr>
+                        </tbody>
+                    </Table>
+                </div>
+            </div>
+        </Container>
     );
 }
 
