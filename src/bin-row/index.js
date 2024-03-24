1762bea87bd219bf3494735b0d2b09ffcc14d497
Doron Chosnek <my.random.items@gmail.com>
2024-03-24 17:51:46 -0500

    moved tooltip locations

--- src/bin-row/index.js
+++ src/bin-row/index.js
@@ -7,7 +7,7 @@ import Tooltip from 'react-bootstrap/Tooltip';
 
 import { PencilSquare, Floppy, Trash } from 'react-bootstrap-icons';
 
-// Create a single row for a table with "binId", "contents", and action buttons
+// Create a single row for a table with 'binId', 'contents', and action buttons
 // as the three columns. The action buttons allow a user to edit the contents
 // of that row's bin, save their edits, or delete that bin entirely.
 // props: binId, binContents
@@ -31,17 +31,17 @@ function BinRow(props) {
             <td hidden={!editMode}><input type='text' value={contents} size={contents.length} onChange={(event) => { setContents(event.target.value) }} /></td>
             {/* column 3: action buttons (edit, save, delete) */}
             <td>
-                <OverlayTrigger placement='top' overlay={<Tooltip>Edit</Tooltip>}>
-                    <Button variant="outline-dark" className='button-pad' hidden={editMode} onClick={() => { setEditMode(true) }}><PencilSquare /></Button>
+                <OverlayTrigger placement='left' overlay={<Tooltip>Edit</Tooltip>}>
+                    <Button variant='outline-dark' className='button-pad' hidden={editMode} onClick={() => { setEditMode(true) }}><PencilSquare /></Button>
                 </OverlayTrigger>
-                <OverlayTrigger placement='top' overlay={<Tooltip>Save</Tooltip>}>
-                    <Button variant="outline-dark" className='button-pad' hidden={!editMode} onClick={() => {
+                <OverlayTrigger placement='left' overlay={<Tooltip>Save</Tooltip>}>
+                    <Button variant='outline-dark' className='button-pad' hidden={!editMode} onClick={() => {
                         setEditMode(false);
                         props.saveFunc(props.binId, contents);
                     }}><Floppy /></Button>
                 </OverlayTrigger>
-                <OverlayTrigger placement='top' overlay={<Tooltip>Delete</Tooltip>}>
-                    <Button variant="outline-danger" onClick={() => props.delFunc(props.binId)}><Trash /></Button>
+                <OverlayTrigger placement='right' overlay={<Tooltip>Delete</Tooltip>}>
+                    <Button variant='outline-danger' onClick={() => props.delFunc(props.binId)}><Trash /></Button>
                 </OverlayTrigger>
             </td>
         </tr>
