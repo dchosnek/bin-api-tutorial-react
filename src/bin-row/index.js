e13e9f310fcb5b98b2fc80641c6694e98f78a9ae
Doron Chosnek <my.random.items@gmail.com>
2024-03-30 17:43:00 -0500

    edit mode improvements

--- src/bin-row/index.js
+++ src/bin-row/index.js
@@ -1,5 +1,5 @@
 import './bin-row.css';
-import { useState, useEffect } from 'react';
+import { useState, useEffect, useRef } from 'react';
 
 import Button from 'react-bootstrap/Button';
 import OverlayTrigger from 'react-bootstrap/OverlayTrigger';
@@ -15,6 +15,9 @@ function BinRow(props) {
 
     const [editMode, setEditMode] = useState(false);
     const [contents, setContents] = useState(props.binContents);
+    // track the state before entering edit mode so we only perform an API
+    // call when there is a change in contents
+    const [beforeEdits, setBeforeEdits] = useState(props.binContents);
 
     // this is needed to refresh the row if the contents ever change
     useEffect(() => {
@@ -22,23 +25,61 @@ function BinRow(props) {
         setEditMode(false);
     }, [props])
 
+    // create a reference to the input text box in order to place the cursor
+    // in it in edit mode
+    const inputRef = useRef(null);
+
+    // look for user to hit the enter key while typing in the input box
+    const handleKeyDown = (event) => {
+        if (event.key === 'Enter') {
+            exitEditMode();
+        }
+    };
+
+    // handle exiting edit mode (whether by pressing save or hitting enter)
+    // make an API call only if the contents of the field have changed
+    const exitEditMode = () => {
+        setEditMode(false);
+        if (contents !== beforeEdits) {
+            props.saveFunc(props.binId, contents);
+            setBeforeEdits(contents);
+        }
+    };
+
+    // handle entering edit mode, which involves turning on edit mode but also
+    // putting the cursor at the end of the input box, which proved tricky for
+    // Chrome/Brave (had to add a setTimeout)
+    const enterEditMode = () => {
+        // this will make the input box visible
+        setEditMode(true);
+        //timeout is required to ensure rendering is complete
+        // before attempting to set focus on the input box
+        setTimeout(() => {
+            const input = inputRef.current;
+            if (input) {    // make sure the ref is not null
+                input.focus();
+                // set the cursor position to the end of text
+                const length = input.value.length;
+                input.setSelectionRange(length, length);
+            }
+        }, 0);
+    }
+
     return (
         <tr>
             {/* column 1 */}
             <td>{props.binId}</td>
             {/* column 2: either bin contents or an input box to edit the contents */}
             <td hidden={editMode}>{contents}</td>
-            <td hidden={!editMode}><input type='text' value={contents} size={contents.length} onChange={(event) => { setContents(event.target.value) }} /></td>
+            <td hidden={!editMode}><input type='text' value={contents} ref={inputRef} size={contents.length} onKeyDown={handleKeyDown} onFocus={() => { console.log('focus!') }} onChange={(event) => { setContents(event.target.value) }} /></td>
             {/* column 3: action buttons (edit, save, delete) */}
             <td>
                 <OverlayTrigger placement='left' overlay={<Tooltip>Edit</Tooltip>}>
-                    <Button variant='outline-light' className='button-pad' hidden={editMode} onClick={() => { setEditMode(true) }}><PencilSquare /></Button>
+                    <Button variant='outline-light' className='button-pad' hidden={editMode} onClick={enterEditMode}><PencilSquare />
+                    </Button>
                 </OverlayTrigger>
                 <OverlayTrigger placement='left' overlay={<Tooltip>Save</Tooltip>}>
-                    <Button variant='outline-light' className='button-pad' hidden={!editMode} onClick={() => {
-                        setEditMode(false);
-                        props.saveFunc(props.binId, contents);
-                    }}><Floppy /></Button>
+                    <Button variant='outline-light' className='button-pad' hidden={!editMode} onClick={exitEditMode}><Floppy /></Button>
                 </OverlayTrigger>
                 <OverlayTrigger placement='right' overlay={<Tooltip>Delete</Tooltip>}>
                     <Button variant='outline-danger' onClick={() => props.delFunc(props.binId)}><Trash /></Button>
