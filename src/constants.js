2f4da63fe5b9ee685e1842b32efc55c095d8aaa5
Doron Chosnek <my.random.items@gmail.com>
2024-09-13 07:58:31 -0500

    moved API IP addr to JSON file

--- src/constants.js
+++ src/constants.js
@@ -1,1 +1,12 @@
-export const API_BASE_URL = process.env.REACT_APP_API_BASE_URL;
+export let API_BASE_URL = '';
+
+export const loadConfig = async () => {
+  try {
+    const response = await fetch('/config.json');
+    const config = await response.json();
+    API_BASE_URL = config.REACT_APP_API_BASE_URL;
+    console.log("API Base URL set to:", API_BASE_URL);
+  } catch (error) {
+    console.error('Error loading config.json:', error);
+  }
+};
--- src/index.js
+++ src/index.js
@@ -1,18 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { loadConfig } from './constants';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
+// Create the root for React 18
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+
+// Load the config before rendering the app
+loadConfig().then(() => {
+  // After the config is loaded, render the React app
+  root.render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  );
+}).catch((error) => {
+  console.error("Failed to load config, app won't start:", error);
+});
+
